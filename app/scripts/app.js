53042d4cdcacc5c23a868dfef7a18b885e4e7c6b
Chammz <topher.hammond@gmail.com>
2017-01-02 13:57:34 -0700

    Complete. Created controller for collection view and setup access to collection template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,8 +9,8 @@
        $stateProvider
           .state('landing', {
             url: '/',
+            controller: 'LandingCtrl as landing',
             templateUrl: '/templates/landing.html'
-
           })
           .state('album', {
             url: '/album',
@@ -18,6 +18,7 @@
           })
           .state('collection', {
             url: '/collection',
+            controller: 'CollectionCtrl as collection',
             templateUrl: '/templates/collection.html'
           });
      }
